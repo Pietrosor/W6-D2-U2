9f810ad330964499a73093690aea62123f6ae511
Pietrosor <pietrosorbo@hotmail.it>
2025-06-10 15:17:18 +0200

    Initial commit

---
+++ W6-D2-U2/src/main/java/it/epicode/W6_D2_U2/W6D2U2Application.java
@@ -0,0 +1,13 @@
+package it.epicode.W6_D2_U2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W6D2U2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W6D2U2Application.class, args);
+	}
+
+}
---
+++ W6-D2-U2/src/test/java/it/epicode/W6_D2_U2/W6D2U2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.W6_D2_U2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6D2U2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
